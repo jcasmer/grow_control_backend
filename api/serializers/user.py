e3478b639eca102486c1ec1d5cdd128eb821ef66
Juan Esteban <juanes3101@gmail.com>
2018-05-10 21:35:40 -0500

    added cofirm_password in serializers and overwrite initto validate when is update

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -18,6 +18,25 @@ class UserSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(label='Nombre(s)', required=True, allow_null=False)
     last_name = serializers.CharField(label='Apellido(s)', required=True, allow_null=False)
     password = serializers.CharField(label='Contraseña', required=True, allow_null=False, style={'input_type': 'password'}, write_only=True)
+    confirm_password = serializers.CharField(label='Confirme contraseña', required=False, allow_null=True, allow_blank=True, style={'input_type': 'password'}, write_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(kwargs)
+        if 'data' in kwargs:
+            if 'partial' in kwargs:
+                self.fields['password'].required = False
+                self.fields['password'].allow_blank = True
+                self.fields['confirm_password'].required = False
+                self.fields['confirm_password'].allow_blank = True
+            else:
+                self.fields['password'].required = True
+                self.fields['password'].allow_blank = False
+                self.fields['password'].allow_null = False
+                self.fields['confirm_password'].required = True
+                self.fields['confirm_password'].allow_blank = False
+                self.fields['confirm_password'].allow_null = False
+
 
     def create(self, validated_data):
 
@@ -35,8 +54,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = User
-        fields = ('url', 'id', 'username', 'password', 'first_name',
-                  'last_name', 'email', 'is_active', 'is_superuser', 'is_staff')
+        fields = ('url', 'id', 'username', 'password', 'confirm_password', 'first_name',
+                  'last_name', 'email', 'is_active', 'is_superuser', 'is_staff', 'groups')
         extra_kwargs = {
             'username': {
                 'validators': [
