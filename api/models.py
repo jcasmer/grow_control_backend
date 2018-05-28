d9eaaf4e0f17871fdf745a42244ff4b10a4c0b7c
Juan Esteban <juanes3101@gmail.com>
2018-05-27 20:32:31 -0500

    added validations models

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from lib.validators import isnumbervalidator
 from src.model import BaseModel
 
 GENDER_TYPE = (
@@ -74,7 +75,8 @@ class Parents(BaseModel):
     )
 
     document_type = models.CharField('Tipo de documento', max_length=100, choices=DOCUMENT_TYPE)
-    document = models.CharField('Documento', max_length=20, unique=True, error_messages={'unique': UNIQUE_DOCUMENT_MESSAGE})
+    document = models.CharField('Documento', max_length=20, unique=True, 
+            error_messages={'unique': UNIQUE_DOCUMENT_MESSAGE}, validators=[isnumbervalidator])
     name = models.CharField('Nombre', max_length=150)
     age = models.CharField('Edad', max_length=3)
     gender = models.CharField('Genero', max_length=50, choices=GENDER_TYPE)
